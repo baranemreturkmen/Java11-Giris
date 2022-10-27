5d1004c197e83f82f600b8962490c9e59b8e2697
Baran Emre Turkmen <baranemre1997@gmail.com>
2022-10-27 21:55:34 +0300

    Java İleri projesi kapsamında stream api çalışmaları yapıldı.

--- stream-api-exercices-part1/src/com/example/exercises/Exercise1.java
+++ stream-api-exercices-part1/src/com/example/exercises/Exercise1.java
@@ -1,7 +1,11 @@
 package com.example.exercises;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import com.example.domain.Director;
 import com.example.domain.Movie;
 import com.example.service.InMemoryMovieService;
 import com.example.service.MovieService;
@@ -18,6 +22,64 @@ public class Exercise1 {
 		// Find the number of movies of each director
         final Collection<Movie> movies = movieService.findAllMovies();
 
+		//Her bir yönetmenin kaç tane film çektiğini bulmak istiyoruz.
+		var numOfMoviesOfEachDirector = //Biz var diyoruz ama Compiler Map<Director,Long> dönüyor.
+		movies.stream()						//Stream<Movie>
+				.map(Movie::getDirectors)   //Stream<List<Director>>
+				.flatMap(List::stream) 		//Stream<Director> flatMap yapılacak bu işleme flattening deniyor. Elimizde listeler var onları yan yatırıcaz stream of director elde edicez.
+				.collect(Collectors.groupingBy(Function.identity(),Collectors.counting())); //Doğrudan direktöre göre gruplama yapıcaz identity yeterli, her grup için sayıyı da reduce edicez.
+
+		/*Identity kullanmamazın sebebi şu yönetmeni yönetmen yapan şey nesnenin bellekteki adresi mantığıyla yola çıkıyoruz.
+		* Şöyle bir durum da olabilirdi */
+
+		var numOfMoviesOfEachDirector2 = //Compiler Map<String,Long> dönüyor bu sefer.
+				movies.stream()
+						.map(Movie::getDirectors)
+						.flatMap(List::stream)
+						.collect(Collectors.groupingBy(d -> d.getName(),Collectors.counting()));
+
+		/*Ama aynı isimde başka directorlar da olabilirdi ve işler burada çok karışacaktı. Ve tabi ki başka özelliklere
+		* göre de gruplama yapabilirim.*/
+
+		var numOfMoviesOfEachDirector3 = //Compiler Map<String,Long> dönüyor bu sefer.
+				movies.stream()
+						.map(Movie::getDirectors)
+						.flatMap(List::stream)
+						.collect(Collectors.groupingBy(d -> d.getImdb(),Collectors.counting()));
+
+		//Veya identity kullanmak yerine d -> d yazabilirim ama çirkin bir kullanım.
+
+		/*HashCode kullanabilirim ama gruplayınca integer değer dönecek yani gruplamış oldu ama kim o
+		  dolayısıyla identity kullanarak doğrudan director'un kendisine göre gruplamak daha mantıklı. Burada key value
+		  yapıyorum aslında key Director, value Long ve ben Director key'i üzerinden birçok bilgiye ulaşabilirim.*/
+
+		//Yukarıdaki ilk çözümü daha da kısaltabilirim.
+
+		var numOfMoviesOfEachDirector4 = //Compiler Map<Director,Long> dönüyor bu sefer.
+				movies.stream()
+						//.map(Movie::getDirectors)
+						.flatMap(movie -> movie.getDirectors().stream())//Yukarıdakine göre tek bir metodda Stream<Director>'a geçiyor.
+						.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+		//Statik import yapıp yine kodları kısaltabilirim.
+
+		//.collect(groupingBy(identity(),counting())) şeklinde.
+
+		//key ve value'yu forEach ile döndürüyorum. key -> Director, value -> count
+		numOfMoviesOfEachDirector.forEach((director, count) -> System.out.println(director.getName()+": "+count));
+
+		Function<Director,String> byName = Director::getName;
+		//Function<String,Director> byImdb = u -> ...; bu yapı olsaydı eğer kodlarda aşağıdaki gibi andThen kullanımı yapabilirdik.
+		//Gruplama yap isme göre sonrada Imdb'ye göre grupla tarzında.
+
+		//Böyle de bir kullanım mevcut.
+		Function<Director,Director> byDirector = d -> d;
+
+		/*var numOfMoviesOfEachDirector5 =
+				movies.stream()
+						//.map(Movie::getDirectors)
+						.flatMap(movie -> movie.getDirectors().stream())
+						.collect(Collectors.groupingBy(byName.andThen(byImdb),Collectors.counting()));*/
 	}
 
 }
--- stream-api-exercices-part1/src/com/example/exercises/Exercise2.java
+++ stream-api-exercices-part1/src/com/example/exercises/Exercise2.java
@@ -2,6 +2,10 @@ package com.example.exercises;
 
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
+import com.example.domain.Country;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 /**
  * 
@@ -12,7 +16,53 @@ public class Exercise2 {
 	private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
 
 	public static void main(String[] args) {
-		// Find the most populated city of each continent
+		// Find the most populated country of each continent
+
+		var mostPopulatedCityOfEachContinent =
+				countryDao.findAllCountries()
+						  .stream()
+						  .collect(Collectors.groupingBy(Country::getContinent
+								,Collectors.maxBy(Comparator.comparing(Country::getPopulation))));
+
+		//Yukarıda bir liste dönmüyor maxBy ile reduce ediyorum. Sadece en buyuk olani bana ver diyorum.
+
+		mostPopulatedCityOfEachContinent
+				.forEach((continent, country)
+						-> country
+						.ifPresent(ctry -> System.out.println(continent+": "+ctry.getName())));
+
+		//Yada direk tüm bilgileri sayısal olarak görmek istersem böyle de yazdırabilirim yukarıda lambda expressionla uğraşmadan
+		System.err.println(mostPopulatedCityOfEachContinent);
+
+		System.out.println("------------------------------");
+
+		//Kıtalardaki tüm ülkeleri yazdıralım. Kıtalardaki ülkeler liste olarak dönecek. Herbir liste elemanı ülkeye ait bilgileri içinde bulunduracak.
+
+		//???Burada neden sadece ülkeye ait name ve population dönüyor listelerin içerisinde ülkeye ait bir sürü özellik var neden böyle anlamadım???
+		countryDao.findAllCountries()
+				.stream()
+				.collect(Collectors.groupingBy(Country::getContinent))
+				.forEach((continent, country)
+						-> System.out.println(continent+": "+country));
+
+		//Tüm asya ülkelerini bul.
+		var asianCountries = countryDao
+				.findAllCountries()
+				.stream()
+				.filter(country -> "Asian".equals(country.getContinent()))
+				//.toList() özel bir reduce işlemi Java16 ile geldi.
+				.collect(Collectors.toList());
+
+		/*Java8 geldi tüm sorunları functional programming ile çözmeliyim diye birşey yok. Bazı durumlarda klasik for
+		* döngüsüyle iflerle sorunları daha kolay çözebilirim. Java'da fonksiyonel proglamlamanın diğer programlama
+		* dillerine göre bir farkı var o da şu tamamen multi core programlama yapabilirim. Multi core'da thread programlamanın
+		* karmaşıklığı ile uğraşmadan problemi çözebiliriz. Hiç paralel programlama bilmeden multi core çalışabiliyorum stream api
+		* bize bu konuda çözüm sunabiliyor. Bundan dolayı çözümlerimi klasik for loop ile yaparken paralelleştirme bu kadar kolay
+		* olmayacak. Tabi stream api paralelleştirme türü olarak data paralellizm ise bir çözüm sunuyor. Farklı paralelleştirme
+		* yöntemleri var. Elimde bir collection api varsa collection api'deki veriler varsa problemim data paralellizm türüne
+		* giren bir problemdir. Onun dışında başka task paralellizm vs. gibi farklı paralellizm türleri var. Collection api,
+		* stream api bize data paralellizm sağlıyor. Bunun alt yapısını sunuyor. Alt yapısı Java7'de geldi. ForkJoinPool dediğimiz
+		* Java7 ile gelen alt yapı sayesinde parallel programlamayı stream api ile kolay bir şekilde yapabiliyoruz.*/
 	}
 
 }
